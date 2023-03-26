e98817c6700c5fb8879d14ea3d53b2693ea0247b
SkippyMcGoo <122652588+SkippyMcGoo@users.noreply.github.com>
2023-03-26 00:09:23 -0500

    Delete Project3-Satterlee.py

--- Project3-Satterlee.py
+++
@@ -1,6 +0,0 @@
-newies = int(input("Please enter the number of new releases you would like to rent: "))
-newiesTotal = newies * 3.00
-oldies = int(input("Please enter the number of old releases you would like to rent: "))
-oldieTotal = oldies * 2.00
-rentalTotal = round(oldieTotal + newiesTotal)
-print("your total rental price will be:", rentalTotal)
