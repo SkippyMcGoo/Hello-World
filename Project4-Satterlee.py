fa5416fbe3a5c1d2a00fbf52b9010e8328b7194d
SkippyMcGoo <122652588+SkippyMcGoo@users.noreply.github.com>
2023-03-26 00:08:52 -0500

    Delete Project4-Satterlee.py

--- Project4-Satterlee.py
+++
@@ -1,8 +0,0 @@
-rad = float(input("Please enter the radius of the circle: "))
-circumference = 2 * 3.14 * rad
-diameter = 2 * rad
-sa = 4 * 3.14 * (rad ** 2)
-volume = 1.333 * 3.14 * (rad ** 3)
-print("Circumference:", circumference, "Diameter:", diameter, "surface area:", sa, "volume:", volume)
-
-
