35ea8c8545e5c0e4e90f03b62309f31875c57b1b
SkippyMcGoo <122652588+SkippyMcGoo@users.noreply.github.com>
2023-03-26 00:12:57 -0500

    Add files via upload

---
+++ Week1Project2-Satterlee.py
@@ -0,0 +1,4 @@
+name = "Preston Satterlee"
+phoneNumber = "405-301-6484"
+address = "13801 South Independence Ave"
+print (name, phoneNumber, address)
---
+++ Week1Project5-Satterlee.py
@@ -0,0 +1,4 @@
+base = int(input("Enter with base: "))
+height = int(input("Enter the height: "))
+area = base * height
+print("The area is", area, "square units.")
